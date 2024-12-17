f551f835802d805f1d279b035ca5039b637d1302
wizdom972 <jihae0584@gmail.com>
2024-12-17 13:40:13 +0900

    🎉 Servlet & JSP

---
+++ helloweb/src/main/java/helloweb/HelloServlet.java
@@ -0,0 +1,31 @@
+package helloweb;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html; charset=utf-8");
+
+		PrintWriter out = response.getWriter();
+		out.print("<h1>Hello World<h1>");
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		doGet(request, response);
+
+	}
+
+}
